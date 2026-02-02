6818ae1dda1150e75c09083ac89cdcb8abdc7730
Philippe Lefebvre <philippelefebvre1@hotmail.fr>
2026-02-02 10:44:30 -0500

    Debut de projet

---
+++ Src/usermain.c
@@ -0,0 +1,16 @@
+/*
+ * usermain.c
+ *
+ *  Created on: 12 janv. 2026
+ *      Author: plefebvre1
+ */
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+int main(void){
+
+	systemInit();
+
+	return 0;
+}
